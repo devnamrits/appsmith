1d33626c9b8aefd4ea1a29a124e0c13be9c176ed
ohansFavour <fohanekwu@gmail.com>
2022-02-27 20:56:25 +0100

    Restructure active action flow

--- app/client/src/ce/constants/messages.ts
+++ app/client/src/ce/constants/messages.ts
@@ -459,6 +459,10 @@ export const JS_SETTINGS_CONFIRM_EXECUTION_SUBTEXT = () =>
   "Ask confirmation from the user every time before refreshing data";
 export const JS_SETTINGS_EXECUTE_TIMEOUT = () =>
   "Function Timeout (in milliseconds)";
+export const NO_JS_FUNCTION_TO_RUN = (
+  JSObjectName: string,
+) => `${JSObjectName} has no function,
+  please add a function to run.`;
 
 // Import/Export Application features
 export const IMPORT_APPLICATION_MODAL_TITLE = () => "Import application";
@@ -1040,7 +1044,3 @@ export const CONTEXT_MOVE = () => "Move to page";
 export const CONTEXT_COPY = () => "Copy to page";
 export const CONTEXT_DELETE = () => "Delete";
 export const CONTEXT_NO_PAGE = () => "No pages";
-export const NO_JS_FUNCTION_TO_RUN = (
-  JSObjectName: string,
-) => `${JSObjectName} has no function,
-please create a function.`;
